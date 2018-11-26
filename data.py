6778953db6b3aefd34fe7763a1c349e517b58bcc
bill <williamandrewbain@gmail.com>
2018-11-26 09:41:49 -0700

    Initial commit

---
+++ data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles = [
+        {
+        'id': 1,
+        'title': 'Article one',
+        'body': 'This is an article. the first one',
+        'author': 'Peter',
+        'create_date': '11-24-18'
+        },
+        {
+        'id': 2,
+        'title': 'Article two',
+        'body': 'This is another article. Second one',
+        'author': 'Bruce',
+        'create_date': '11-24-18'
+        }
+    ]
+    return articles
